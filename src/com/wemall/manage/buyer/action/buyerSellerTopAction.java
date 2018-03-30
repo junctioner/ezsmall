621362c30e0d0e481ebbc4199497892e81f64029
廖武威 <@LAPTOP-Q6688MJU>
2018-03-30 20:39:15 +0800

    提交买家头部左部样式

---
+++ src/com/wemall/manage/buyer/action/buyerSellerTopAction.java
@@ -0,0 +1,41 @@
+package com.wemall.manage.buyer.action;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.wemall.core.mv.JModelAndView;
+import com.wemall.foundation.service.ISysConfigService;
+import com.wemall.foundation.service.IUserConfigService;
+
+@Controller
+public class buyerSellerTopAction {
+	@Autowired
+	private ISysConfigService configService;
+
+	@Autowired
+	private IUserConfigService userConfigService;
+	
+	@RequestMapping("/seller/buyer_top.htm")
+	public ModelAndView buyer_top(String id, HttpServletRequest request, HttpServletResponse response) {
+		ModelAndView mv = new JModelAndView("/default/buyer/buyer_top.html", this.configService.getSysConfig(),
+				this.userConfigService.getUserConfig(), 1, request, response);
+		return mv;
+	}
+	@RequestMapping("/seller/buyer_info_left.htm")
+	public ModelAndView buyer_info_left(String id, HttpServletRequest request, HttpServletResponse response) {
+		ModelAndView mv = new JModelAndView("/default/buyer/buyer_info_left.html", this.configService.getSysConfig(),
+				this.userConfigService.getUserConfig(), 1, request, response);
+		return mv;
+	}
+	@RequestMapping("/seller/buyer_link.htm")
+	public ModelAndView buyer_link(String id, HttpServletRequest request, HttpServletResponse response) {
+		ModelAndView mv = new JModelAndView("/default/buyer/buyer_link.html", this.configService.getSysConfig(),
+				this.userConfigService.getUserConfig(), 1, request, response);
+		return mv;
+	}
+}
