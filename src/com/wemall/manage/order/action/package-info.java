f3aed949d35f7aa1d36c9d32401f66e73cba403e
whggbdqn <whggbdqn@163.com>
2018-03-30 15:55:38 +0800

    添加目录说明

---
+++ src/com/wemall/manage/order/action/package-info.java
@@ -0,0 +1,15 @@
+/**
+ * Project Name:wemall2
+ * File Name:package-info.java
+ * Package Name:com.wemall.manage.order.action
+ * Date:2018年3月30日上午11:08:34
+ * Copyright (c) 2018, jungle All Rights Reserved.
+ */
+/**
+ * Description: 后台订单管理Controller类包<br>
+ * date: 2018年3月30日 上午11:08:34 <br>
+ *
+ * @author thinkpad
+ * @version
+ */
+package com.wemall.manage.order.action;
