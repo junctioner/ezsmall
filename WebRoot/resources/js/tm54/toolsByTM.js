bfe76f3b8e7866c77998f68fa89fd64972816133
tumin <tumin@TM>
2018-04-01 13:11:36 +0800

    涂敏 04-01 js自动隐藏提示框更新，可设置参数：提示信息，背景颜色，字体颜色,字体大小(大，中，小)，显示时间 示例: var arg = { 				message:"时间选择错误!", 				bgc:"#369", 				color:"#fff", 				fontsize:"中", 				time: 3 			}; 		  errorAlertByTM(arg); js文件位置：WebRoot/resources/js/tm54/toolsByTM.js

--- WebRoot/resources/js/tm54/toolsByTM.js
+++ WebRoot/resources/js/tm54/toolsByTM.js
@@ -1,11 +1,41 @@
 /*错误提示框(自动隐藏)*/
-/*参数为提示信息,背景颜色，字体颜色*/
-function errorAlertByTM(message, bgc, color) {
-	var $ErrorAlert = jQuery("<div id='TMByError'><h3>" + message + "</h3></div>");
-	jQuery("body").append($ErrorAlert);
-	jQuery("#TMByError").css({
-		"transition": "opacity 1.5s",
-		"background-color": bgc,
+/*参数为提示信息,背景颜色，字体颜色,字体大小，完全消失时间*/
+function errorAlertByTM(arg) {
+	var number = /^[0-9]*[.]?[0-9]*$/;
+	var size;
+	//判断参数是否为集合，不是则改为空集合
+	if(Object.prototype.toString.call(arg) != '[object Object]') {
+		arg = {};
+	}
+	//判断是否有提示文字，没有则提示错误
+	if(arg.message == null || arg.message == "" || arg.message == undefined) {
+		arg.message = "错误";
+	}
+	//判断是否有正确的背景颜色，没有则默认#aaa
+	if(arg.bgc == null || arg.bgc == "" || arg.bgc == undefined) {
+		arg.bgc = "#aaa";
+	}
+	//判断是否有正确的背景颜色，没有则默认#aaa
+	if(!number.test(arg.time)) {
+		arg.time = 1.5;
+	}
+	//判断是否有正确的字体大小，没有则默认为中等(18px)
+	if(arg.fontsize == "大") {
+		size = 25;
+	} else if(arg.fontsize == "小") {
+		size = 15;
+	} else {
+		size = 18;
+	}
+	//如果第一次弹出则在body添加提示框
+	if(jQuery("body").children(".TMByError").length<=0){
+		var jQueryErrorAlert = jQuery("<div class='TMByError'><span>" + arg.message + "</span></div>");
+	}
+	jQuery("body").append(jQueryErrorAlert);
+	//写入css样式
+	jQuery(".TMByError").css({
+		"transition": "opacity " + arg.time + "s",
+		"background-color": arg.bgc,
 		"display": "inline-block",
 		"text-align": "center",
 		"opacity": "1",
@@ -13,14 +43,18 @@ function errorAlertByTM(message, bgc, color) {
 		"position": "fixed",
 		"left": "45%",
 		"top": "45%",
-		"color": color,
+		"color": arg.color,
 		"padding": "5px",
-		"overflow": "auto"
+		"overflow": "auto",
+		"font-size": size + "px",
+		"font-weight": "bold"
 	}).show();
+	//arg.time/2秒后开始消失
 	setTimeout(function() {
-		jQuery("#TMByError").css("opacity", "0");
-	}, 1500);
+		jQuery(".TMByError").css("opacity", "0");
+	}, arg.time * 500);
+	//arg.time秒后完全消失
 	setTimeout(function() {
-		jQuery("#TMByError").remove();
-	}, 3000);
+		jQuery(".TMByError").hide();
+	}, arg.time * 1000);
 }
